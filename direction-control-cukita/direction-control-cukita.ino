7222585a7b9d8295460b789ff5e894c8586c786d
Keriz <guillaume.thivolet@epfl.ch>
2018-08-27 15:57:37 +0200

    cleaning up the file

--- direction-control-cukita/direction-control-cukita.ino
+++ direction-control-cukita/direction-control-cukita.ino
@@ -1,124 +1,137 @@
-#define pinIV  A5 //inversion motor direccion 
-#define pinODOMETRO 2 // cuenta inpulsiones de la rueda /una vuelta de rueda 88cm 14 cm por pulso 
-#define pinANGULO A4 // sensor de angulo de direccion de ruedas
-#define pinIS A2 //comando isquierda joisty 
-#define pinDE A3 // comando derecha joisty
-#define pinH A1 //homing 
-#define pinMA A0 // boton de marcha y arret manual  
-#define pinM 8  // reley motor on/off
-#define pinA 4  // reley 1
-#define pinB 7  // reley 2
-#define LedVerde 11 // comando a la derecha
-#define LedRojo 10 // comando a la isquierda
+//#############
+//PIN DEFINITION
+#define PIN_JOYSTICK_LEFT 2 //comando isquierda joisty
+#define PIN_JOYSTICK_RIGHT 3 // comando derecha joisty
+#define PIN_MOTOR_RELAY_1 4  // relay 1
+#define PIN_HALL_WHEEL 5 //una vuelta de rueda 88cm 14 cm por pulso
+#define PIN_HALL_HOMING 6 //homing hall sensor
+#define PIN_MOTOR_RELAY_2 7  // relay 2
+#define PIN_MOTOR_SWITCH 8  // reley motor on/off
+#define PIN_AUTOMATIC_MODE A0 // button to activate response to line detection
+#define PIN_ANGLE A4 // rotative encoder for wheel angle
 
-// int pinANGULO A4 ;
-int option;
-int T1 =60 ; // factor de multiplicacion del tiempo 
-int pos=99; 
-String readString;
-int val = 0;       // variable que almacena el valor raw (0 a 1023)
+//#############
+//CONSTANTS DEFINITION
+#define HOMING_STEPS 200
+#define ANGLE_TOLERANCE 2 //in degrees
+#define ROTATION_STEP_DELAY 40 //in ms
+#define FALSE_ANGLE_TOLERANCE 50
+#define ANGLE_CONTINUITY_LIMIT 20
+#define WHEEL_ANGLE_ZERO 480 //calibration needed
 
+uint32_t wheelAngle=0, oldAngle=0, newAngle=0;
+
+void getWheelAngle(void){
+  wheelAngle = analogRead(PIN_ANGLE);
+  wheelAngle = ((wheelAngle-WHEEL_ANGLE_ZERO)/2); //have to define 2 has a constant
+  wheelAngle *= -1; //inversing the angle for later
+}
+
+void stopTurning(void){
+  digitalWrite(PIN_MOTOR_RELAY_1,LOW);
+  digitalWrite(PIN_MOTOR_RELAY_2,LOW);
+}
+
+void turnRight(void){
+  digitalWrite(PIN_MOTOR_RELAY_1,HIGH);
+  digitalWrite(PIN_MOTOR_RELAY_2,LOW);
+}
+
+void turnLeft(void){
+  digitalWrite(PIN_MOTOR_RELAY_1,LOW);
+  digitalWrite(PIN_MOTOR_RELAY_2,HIGH);
+}
+
+void homingProcedure(){
+    for (unsigned char i=0; i<HOMING_STEPS; i++){
+      if(!digitalRead(PIN_HALL_HOMING))
+        break;
+      if (i<(HOMING_STEPS/2))
+        turnRight();
+      else
+        turnLeft();
+       delay(ROTATION_STEP_DELAY);
+       stopTurning();
+    }
+    getWheelAngle();
+}
 
 void setup() {
   Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(PIN_JOYSTICK_LEFT),
+                  joystickLeftInterrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_JOYSTICK_RIGHT),
+                  joystickRightInterrupt, CHANGE);
+  pinMode(PIN_MOTOR_RELAY_1,OUTPUT);
+  pinMode(PIN_MOTOR_RELAY_2,OUTPUT);
+  pinMode(PIN_MOTOR_SWITCH,OUTPUT);
+  pinMode(PIN_AUTOMATIC_MODE,INPUT_PULLUP);
+  pinMode(PIN_HALL_HOMING,INPUT_PULLUP);
+  homingProcedure();
+  oldAngle = wheelAngle;
+}
 
- // Serial.println("servo-test-22-dual-input"); // so I can keep track of what is loaded
-  pinMode(LedRojo, OUTPUT);
-  pinMode(pinA,OUTPUT); // relay
-  pinMode(pinB,OUTPUT); // relay
-  pinMode(LedVerde,OUTPUT);
-  pinMode(pinM,OUTPUT); // motor
-  pinMode(pinMA,INPUT_PULLUP); // boton 
-  pinMode(pinH,INPUT_PULLUP); // Homing 
-  pinMode(pinDE,INPUT_PULLUP); // comando derecha joisty
-  pinMode(pinIS,INPUT_PULLUP); // comando isquierd josty
-  pinMode(pinIV,INPUT_PULLUP); // invercion comenado dirrecion 
-  
-  for (int i= 0; i<100;i ++){
-       digitalWrite(pinA,HIGH);
-       digitalWrite(pinB,LOW); // aciona actuador  isquierda 
-       delay(50);
-       digitalWrite(pinA,LOW);
-       digitalWrite(pinB,LOW);
-       if(!digitalRead(pinH)){ pos=0;
-       break;}
-    }
-  if (pos>0) {
-     for (int i= 0; i<100;i ++){
-       digitalWrite(pinA,LOW);
-       digitalWrite(pinB,HIGH); // aciona actuador  isquierda 
-       delay(50);
-       digitalWrite(pinA,LOW);
-       digitalWrite(pinB,LOW);
-       if(!digitalRead(pinH)){ pos=0;
-       break;}
-    }
-    
+void joystickLeftInterrupt(void){ //relay 1
+  if (!digitalRead(PIN_AUTOMATIC_MODE))
+    digitalWrite(PIN_MOTOR_SWITCH,HIGH);
+  else{
+    digitalWrite(PIN_MOTOR_SWITCH,LOW);
+    if (digitalRead(PIN_JOYSTICK_LEFT))
+      digitalWrite(PIN_MOTOR_RELAY_1,HIGH);
+    else
+      digitalWrite(PIN_MOTOR_RELAY_2,LOW);
+    digitalWrite(PIN_MOTOR_RELAY_1,LOW);
   }
- // if (pos>0){Serial.println("mierda");}
+}
+
+void joystickRightInterrupt(void){ //relay 2
+  if (!digitalRead(PIN_AUTOMATIC_MODE)) //joystick
+    digitalWrite(PIN_MOTOR_SWITCH,HIGH);
+  else{
+    digitalWrite(PIN_MOTOR_SWITCH,LOW);
+    if (digitalRead(PIN_JOYSTICK_RIGHT))
+      digitalWrite(PIN_MOTOR_RELAY_1,HIGH);
+    else
+      digitalWrite(PIN_MOTOR_RELAY_1,LOW);
+    digitalWrite(PIN_MOTOR_RELAY_2,LOW);
+  }
+}
+
+void goToAngle(int angle){
+  if (abs(angle) > FALSE_ANGLE_TOLERANCE)
+    return; // skip false angles
+  if (abs(angle - oldAngle) >= ANGLE_CONTINUITY_LIMIT)
+    return; // don't obey to not continuous information
+  else
+    oldAngle = angle;
+
+    while(((angle - wheelAngle) <= -ANGLE_TOLERANCE)
+        || ((angle - wheelAngle) >= ANGLE_TOLERANCE) ){
+    getWheelAngle();
+    if (angle - wheelAngle > 0)
+      turnRight();
+    else if (angle - wheelAngle < 0)
+      turnLeft();
+    delay(ROTATION_STEP_DELAY);
+  }
+  stopTurning();
 }
 
 void loop() {
- // val = analogRead(pinANGULO);   // realizar la lectura
- //  Serial.println(val);
-  if (!digitalRead(pinMA)){digitalWrite(pinM,HIGH);}else {digitalWrite(pinM,LOW);  
-  if (!digitalRead(pinIS)){digitalWrite(pinB,HIGH);}else {digitalWrite(pinB,LOW);}// comando isquierda Joisty 20-08-18
-  if (!digitalRead(pinDE)){digitalWrite(pinA,HIGH);}else {digitalWrite(pinA,LOW);}// comando derecha Joisty
-  }
-  
- while (Serial.available()&&(digitalRead(pinM))) {
+//  if (!digitalRead(pinMA)){digitalWrite(pinM,HIGH);}else {digitalWrite(pinM,LOW);
+//  if (!digitalRead(pinIS)){digitalWrite(pinB,HIGH);}else {digitalWrite(pinB,LOW);}// comando isquierda Joisty 20-08-18
+//  if (!digitalRead(pinDE)){digitalWrite(pinA,HIGH);}else {digitalWrite(pinA,LOW);}// comando derecha Joisty
+//  }
+  String serial_direction;
+
+  while (Serial.available()&&(!digitalRead(PIN_AUTOMATIC_MODE))) {
     char c = Serial.read();  //gets one byte from serial buffer
-    readString += c; //makes the string readString
+    serial_direction += c; //makes the string readString
     delay(2);  //slow looping to allow buffer to fill with next character
-    String toPrint = readString + "\n";
-    char stringInChar[200];
-    toPrint.toCharArray(stringInChar, 200);
-    Serial.write(stringInChar);
   }
 
-  if (readString.length() >0) {
-   // Serial.println(readString);  //so you can see the captured string 
-    int n = readString.toInt();  //convert readString into a number
-  if (!digitalRead(pinIV)){n=-n; T1=100;} else {T1=60;} 
-     { val = analogRead(pinANGULO);     // read the input pin
-      Serial.println(val); }   
-
-   if (n >pos) 
-   {
-      digitalWrite(pinA,HIGH);
-      digitalWrite(pinB,LOW); // aciona actuador  isquierda
-      digitalWrite(LedRojo,HIGH);
-      //Serial.println(n);
-     // Serial.println("isquierda");
-      delay(abs(n-pos)*T1);
-      digitalWrite(pinA,LOW);
-      digitalWrite(pinB,LOW);
-      digitalWrite(LedRojo,LOW);
-      pos =n;
-     // Serial.println(n);
-   }
-   if (n<pos)
-   {
-    digitalWrite(pinA,LOW);
-    digitalWrite(pinB,HIGH); // aciona actuador  derecha
-    digitalWrite(LedVerde,HIGH);
-    //Serial.println(n);
-  //  Serial.println("derecha");
-    delay(abs(pos-n)*T1);
-    digitalWrite(pinA,LOW);
-    digitalWrite(pinB,LOW);
-    digitalWrite(LedVerde,LOW);
-    pos=n;
- //   Serial.println(n);
-   }
-   if (n==0){
-    digitalWrite(pinA,LOW);
-    digitalWrite(pinB,LOW);
-    digitalWrite(LedVerde,LOW);
-    digitalWrite(LedRojo,LOW);
- //   Serial.println("stop centro");
-   }
- readString=""; //empty for next input
+  if (serial_direction.length()>0){
+    short angle = serial_direction.toInt();  //convert readString into a number
+    goToAngle(angle);
+  }
 }
-}
-
